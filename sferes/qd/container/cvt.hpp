3d02c7399e5a9fab0cfee869eafe335c0b8ca2cf
Jean-Baptiste Mouret <jean-baptiste.mouret@inria.fr>
2018-08-29 10:24:41 +0200

    cache the centroids

--- sferes/qd/container/cvt.hpp
+++ sferes/qd/container/cvt.hpp
@@ -1,6 +1,7 @@
 #ifndef QD_CONTAINER_CVT_HPP
 #define QD_CONTAINER_CVT_HPP
 #include <Eigen/Core>
+#include <sferes/misc/eigen.hpp>
 
 #include "compute_cvt.hpp"
 
@@ -22,11 +23,37 @@ namespace sferes {
 
                 CVT()
                 {
-                    _archive.resize(Params::qd::cvt::n_niches);
-                    _archive_parents.resize(Params::qd::cvt::n_niches);
-                    _centroids = cvt::cvt(Params::qd::dim, Params::qd::cvt::n_niches,
-                        Params::qd::cvt::n_samples, Params::qd::cvt::max_iterations,
-                        Params::qd::cvt::n_restarts, Params::qd::cvt::tolerance);
+                    SFERES_CONST int dim = Params::qd::dim;
+                    SFERES_CONST int n_niches = Params::qd::cvt::n_niches;
+                    _archive.resize(n_niches);
+                    _archive_parents.resize(n_niches);
+
+                    std::string cache_filename = std::string("cache_centroids_")
+                        + boost::lexical_cast<std::string>(dim) + "_"
+                        + boost::lexical_cast<std::string>(n_niches)
+                        + ".bin";
+                    if(!boost::filesystem::exists(cache_filename)) {
+                        std::cerr << "CVT: no centroid cache found ("
+                                  << cache_filename << ") "
+                                  << "=> computing a new CVT (this might take a dozen of minutes!)"
+                                  << std::endl;
+                        _centroids = cvt::cvt(Params::qd::dim, Params::qd::cvt::n_niches,
+                            Params::qd::cvt::n_samples, Params::qd::cvt::max_iterations,
+                            Params::qd::cvt::n_restarts, Params::qd::cvt::tolerance);
+                        std::ofstream ofs(cache_filename.c_str());
+                        boost::archive::binary_oarchive oa_a(ofs);
+                        oa_a << _centroids;
+                        std::cerr << "CVT saved as: " << cache_filename << std::endl;
+                    }
+                    else {
+                        std::cerr << "CVT: using centroid cache: " << cache_filename << std::endl;
+                        std::ifstream ifs(cache_filename.c_str());
+                        boost::archive::binary_iarchive ia_a(ifs);
+                        ia_a >> _centroids;
+                        std::cerr << _centroids.rows() << " centroids loaded from cache" << std::endl;
+                        assert(_centroids.rows() == Params::qd::cvt::n_niches);
+                        assert(_centroids.cols() == dim);
+                    }
                 }
 
                 void get_full_content(std::vector<indiv_t>& content) const
